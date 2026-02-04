e425a0c6611c7834fe22dbf48b9b272ad3c6ca4d
Maksim <i@mpozdnjakov.ru>
2026-02-04 12:07:13 +0300

    Исправил путь к ноде list_users

--- app/repositories/device_repository.py
+++ app/repositories/device_repository.py
@@ -1,6 +1,8 @@
 import time
 from firebase_admin import db
+
 class DeviceRepository:
+
     @staticmethod
     def upsert_device(
         user_id: str,
@@ -8,13 +10,14 @@ class DeviceRepository:
         push_token: str,
         platform: str
     ):
-        ref = db.reference(f"users/{user_id}/devices/{device_id}")
+        ref = db.reference(f"list_users/{user_id}/devices/{device_id}")
         ref.update({
             "pushToken": push_token,
             "platform": platform,
             "pushEnabled": True,
             "lastSeenAt": int(time.time())
         })
+
     @staticmethod
     def disable_push(user_id: str, device_id: str):
         ref = db.reference(f"users/{user_id}/devices/{device_id}")
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,5 +1,7 @@
 from firebase_admin import db
+
 class UserRepository:
+
     """
     Работа с пользователями в RTDB.
     """
@@ -9,17 +11,23 @@ class UserRepository:
         Возвращает список userKey (ключей в users),
         у которых есть my_shared_list с указанным sharedListId.
         """
-        users_ref = db.reference("users")
+        users_ref = db.reference("list_users")
         users_snapshot = users_ref.get()
+
         if not users_snapshot:
             return []
+
         matched_user_keys: list[str] = []
+
         for user_key, user_data in users_snapshot.items():
             shared_lists = user_data.get("my_shared_list")
+
             if not shared_lists:
                 continue
+
             for item in shared_lists.values():
                 if item.get("listId") == shared_list_id:
                     matched_user_keys.append(user_key)
                     break
+
         return matched_user_keys
