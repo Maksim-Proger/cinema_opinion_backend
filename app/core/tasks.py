69dd929d51662c7abf99a627cde1d38eb9e4c01b
Maksim <i@mpozdnjakov.ru>
2026-09-10 13:03:53 +0300

    Создаем базу на сервере.

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from app.usecases.import_premieres import _run_import
+from app.usecases.import_premieres import import_premieres
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +10,7 @@ SYNC_INTERVAL_SECONDS = 24 * 60 * 60
 async def premieres_sync_worker():
     while True:
         try:
-            result = await asyncio.to_thread(_run_import)
+            result = await asyncio.to_thread(import_premieres)
             logger.info("Синхронизация премьер: %s", result)
         except Exception:
             logger.exception("Ошибка синхронизации премьер")
